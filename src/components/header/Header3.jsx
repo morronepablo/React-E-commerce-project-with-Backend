28697b6c9dfb0b18b1f9e52e4fec5790554721f1
morronepablo <morronepablo@gmail.com>
2024-01-07 00:02:19 -0300

    creación menu mobile and Fullscreen header 3

--- src/components/header/Header3.jsx
+++ src/components/header/Header3.jsx
@@ -1,10 +1,12 @@
 import {
   Box,
   Container,
+  Drawer,
   IconButton,
   ListItemIcon,
   ListItemText,
   Typography,
+  useMediaQuery,
   useTheme,
 } from "@mui/material";
 import { useState } from "react";
@@ -20,7 +22,15 @@ import {
   ElectricBikeOutlined,
   LaptopChromebookOutlined,
   MenuBookOutlined,
+  Close,
 } from "@mui/icons-material";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import Links from "./Links";
 
 function Header3() {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -34,12 +44,31 @@ function Header3() {
 
   const theme = useTheme();
 
+  const [state, setState] = useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
   return (
     <Container
       sx={{
         display: "flex",
         alignItems: "center",
         justifyContent: "space-between",
+        mt: 5,
       }}
     >
       <Box>
@@ -120,10 +149,87 @@ function Header3() {
           </MenuItem>
         </Menu>
       </Box>
+      <Links />
+      {useMediaQuery("(max-width:1000px)") && (
+        <IconButton onClick={toggleDrawer("top", true)}>
+          <MenuIcon />
+        </IconButton>
+      )}
+      {/* Menu Mobile */}
+      <Drawer
+        anchor={"top"}
+        open={state["top"]}
+        onClose={toggleDrawer("top", false)}
+        sx={{
+          ".MuiPaper-root.css-1sozasi-MuiPaper-root-MuiDrawer-paper": {
+            height: "100%",
+          },
+        }}
+      >
+        <Box
+          sx={{ width: 444, mx: "auto", mt: 6, position: "relative", pt: 10 }}
+        >
+          <IconButton
+            sx={{
+              ":hover": { color: "red", rotate: "180deg" },
+              transition: "0.3s",
+              position: "absolute",
+              top: 0,
+              right: 10,
+            }}
+            onClick={toggleDrawer("top", false)}
+          >
+            <Close />
+          </IconButton>
 
-      <IconButton>
-        <MenuIcon />
-      </IconButton>
+          {[
+            { mainLink: "Home", subLink: ["Link 1", "Link 2", "Link 3"] },
+            { mainLink: "Mega menu", subLink: ["Link 1", "Link 2", "Link 3"] },
+            {
+              mainLink: "Full screen menu",
+              subLink: ["Link 1", "Link 2", "Link 3"],
+            },
+            { mainLink: "Pages", subLink: ["Link 1", "Link 2", "Link 3"] },
+            {
+              mainLink: "User account",
+              subLink: ["Link 1", "Link 2", "Link 3"],
+            },
+            {
+              mainLink: "Vendor account",
+              subLink: ["Link 1", "Link 2", "Link 3"],
+            },
+          ].map((item) => {
+            return (
+              <Accordion
+                key={item.mainLink}
+                elevation={0}
+                sx={{ bgcolor: "initial" }}
+              >
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>{item.mainLink}</Typography>
+                </AccordionSummary>
+
+                <List sx={{ py: 0, my: 0 }}>
+                  {item.subLink.map((link) => {
+                    return (
+                      <ListItem key={link} sx={{ py: 0, my: 0 }}>
+                        <ListItemButton>
+                          <ListItemText primary={link} />
+                        </ListItemButton>
+                      </ListItem>
+                    );
+                  })}
+                </List>
+              </Accordion>
+            );
+          })}
+        </Box>
+      </Drawer>
+      {/* Fin Menu Mobile */}
     </Container>
   );
 }
